c139e3527ab62a2bbba99ec896357893aaaef003
KuroHsu <kurotanshi@gmail.com>
2018-01-17 23:57:15 +0800

    first commit

---
+++ 06.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div class="hello">Hello {{ msg }}</div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      msg: 'world'
+    };
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  font-size: 2em;
+  color: #f00;
+}
+</style>
+
+
+
+
+
